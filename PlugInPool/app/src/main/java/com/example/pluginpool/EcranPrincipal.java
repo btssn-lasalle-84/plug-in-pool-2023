2bc3c12982e857a99576f4ac79f57635ee0fc45a
Thierry Vaira <tvaira@free.fr>
2023-03-29 11:01:50 +0200

    Revue de code

--- PlugInPool/app/src/main/java/com/example/pluginpool/EcranPrincipal.java
+++ PlugInPool/app/src/main/java/com/example/pluginpool/EcranPrincipal.java
@@ -1,14 +1,96 @@
+/**
+ * @file EcranPrincipal.java
+ * @brief Déclaration de l'activité principale
+ * @author Clément TRICHET
+ */
+
 package com.example.pluginpool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class EcranPrincipal extends AppCompatActivity {
+/**
+ * @class JustFeed
+ * @brief L'activité principale
+ */
+public class EcranPrincipal extends AppCompatActivity
+{
+    /**
+     * Constantes
+     */
+    private static final String TAG = "_EcranPrincipal"; //!< TAG pour les logs (cf. Logcat)
 
+    /**
+     * Attributs
+     */
+
+    /**
+     * Ressources GUI
+     */
+
+    /**
+     * @brief Méthode appelée à la création de l'activité
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate()");
+    }
+
+    /**
+     * @brief Méthode appelée au démarrage après le onCreate() ou un restart
+     * après un onStop()
+     */
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    /**
+     * @brief Méthode appelée après onStart() ou après onPause()
+     */
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    /**
+     * @brief Méthode appelée après qu'une boîte de dialogue s'est affichée (on
+     * reprend sur un onResume()) ou avant onStop() (activité plus visible)
+     */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    /**
+     * @brief Méthode appelée lorsque l'activité n'est plus visible
+     */
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    /**
+     * @brief Méthode appelée à la destruction de l'application (après onStop()
+     * et détruite par le système Android)
+     */
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
     }
 }
