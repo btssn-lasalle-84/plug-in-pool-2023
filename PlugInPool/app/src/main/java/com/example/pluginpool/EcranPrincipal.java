24aad2386f129089c559bf78e66294a8517aae87
alapierre-ops <>
2023-03-29 10:32:41 +0200

    POOL2023-5-creation-du-projet-android, projet PlugInPool créé

---
+++ PlugInPool/app/src/main/java/com/example/pluginpool/EcranPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.pluginpool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EcranPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
