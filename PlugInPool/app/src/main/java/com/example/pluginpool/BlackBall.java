606230c6748f3b338475b6bbf16209f08a8da056
tvaira <tvaira@free.fr>
2023-03-31 18:18:53 +0200

    Revue de code

---
+++ PlugInPool/app/src/main/java/com/example/pluginpool/BlackBall.java
@@ -0,0 +1,16 @@
+package com.example.pluginpool;
+
+/**
+ * @class BlackBall
+ * @brief Définit des caractéristiques du billard Blackball
+ */
+public class BlackBall
+{
+    public static final int NOIRE   = 0; // !< @def Bille noire 0
+    public static final int ROUGE   = 1; // !< @def Bille rouge 1
+    public static final int JAUNE   = 2; // !< @def Bille jaune 2
+    public static final int BLANCHE = 3; // !< @def Bille blanche 3
+    public static final int NB_BILLES_COULEUR =
+      7; // !< @def Nombre de billes d'une même couleur (rouges ou jaunes) 7
+    public static final int NB_POCHES = 6; // !< @def Nombre de poches de la table 6
+}
