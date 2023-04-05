bfd9c0cde1f16bba50e7a0b0c64f64207b2ba11c
alapierre-ops <>
2023-04-05 17:02:08 +0200

    POOL2023-7-creation-de-lactivite-configuration-de-partie, IHM fichier layout défini

---
+++ PlugInPool/app/src/main/java/com/example/pluginpool/ConfigurationMancheActivity.java
@@ -0,0 +1,14 @@
+package com.example.pluginpool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigurationMancheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration_manche);
+    }
+}
