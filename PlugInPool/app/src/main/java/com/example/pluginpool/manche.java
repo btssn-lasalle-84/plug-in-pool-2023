6ff03b9779bebdfacc51002aec07c33daa918033
alapierre-ops <>
2023-04-06 16:16:11 +0200

    POOL2023-22-creation-de-lihm-de-suivi-de-partie, travail en cours

---
+++ PlugInPool/app/src/main/java/com/example/pluginpool/manche.java
@@ -0,0 +1,14 @@
+package com.example.pluginpool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manche extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manche);
+    }
+}
